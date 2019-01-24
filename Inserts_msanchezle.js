7d5b3c6be751ed5b974c3e907f512e7bc2ba5f0f
msanchezle <46994579+msanchezle@users.noreply.github.com>
2019-01-24 12:39:22 +0100

    Update Inserts_msanchezle.js

--- Inserts_msanchezle.js
+++ Inserts_msanchezle.js
@@ -1,2 +1,2 @@
-use knowtech1
-for (var i=0; i < 10000; i++) {db.alumno.insertOne({ IdAlumno: i, Nombre: "Nombre"+i, Apellidos:"Apellidos"+i, DNI: "DNI"+i})}
+use knowtech1;
+for (var i=0; i < 10000; i++) {db.alumno.insertOne({ IdAlumno: i, Nombre: 'Nombre'+i, Apellidos:'Apellidos'+i, DNI: 'DNI'+i});}
